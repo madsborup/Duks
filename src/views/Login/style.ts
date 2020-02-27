279007d2213dbfd6f147a8122ad421a418c1d3a4
work <madsborpe@gmail.com>
2020-02-27 20:03:38 +0100

    Update Google sign-in button

--- src/views/Login/style.ts
+++ src/views/Login/style.ts
@@ -1,6 +1,7 @@
-import styled from 'styled-components'
-import base from '../../components/designSystem/base'
-import { Button } from '../../components/designSystem/button'
+import styled from "styled-components";
+import base from "../../components/designSystem/base";
+import { OutlineButton } from "../../components/designSystem/button";
+import { ReactComponent as GoogleIcon } from "../../assets/svg/Google_G.svg";
 
 export const LoginView = styled.div`
   display: flex;
@@ -19,6 +20,21 @@ export const Container = styled.div`
   padding: ${base.spacing.large}px;
 `;
 
-export const GoogleSigninButton = styled(Button)`
-  margin-top: ${base.spacing.large}px;
+export const Title = styled.span`
+  font-size: 52px;
+  color: ${base.colors.heading};
+  font-weight: 700;
+  margin-bottom: ${base.spacing.large}px;
+`;
+
+export const GoogleSigninButton = styled(OutlineButton)`
+  display: flex;
+  justify-content: flex-start;
+  box-shadow: 0 2px 1px rgba(0,0,0,0.14),0 0 0 1px rgba(0,0,0,0.1);
+  font-family: ${base.font.family.google};
+  border: 0;
+`;
+
+export const GoogleG = styled(GoogleIcon)`
+  margin-right: 24px;
 `;
