fb273016112df6b61f2fc77b4ba0ff3275ed628a
work <madsborpe@gmail.com>
2020-02-27 20:15:48 +0100

    Add popover onClick

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { ProjectData } from "../../actions";
 import { connect } from "react-redux";
-import { PrimaryButton } from "../designSystem/button";
+import PopoverMenu from "../../components/PopoverMenu";
 import { showModal } from "../../actions";
+import { DropdownArrow } from '../../components/designSystem/icons/DropdownArrow'
 import {
   StyledProjectCard,
   ProjectMetaContainer,
   ProjectTitle,
   ProjectDescription,
   ProjectActionsContainer,
-  DropdownIcon,
   OptionsIcon
 } from "./style";
 
@@ -22,15 +22,36 @@ const ProjectCard: React.FC<Props> = (props: Props) => {
   const { title, description, slug } = props.project;
 
   return (
-    <StyledProjectCard>
-      <ProjectMetaContainer>
-        <ProjectTitle>{title}</ProjectTitle>
-        <ProjectDescription>{description}</ProjectDescription>
-      </ProjectMetaContainer>
-      <ProjectActionsContainer>
-        <DropdownIcon />
-      </ProjectActionsContainer>
-    </StyledProjectCard>
+    <PopoverMenu
+      placement="bottom-end"
+      items={[
+        {
+          type: "option",
+          label: "Settings",
+          onClick: () => console.log("something")
+        },
+        {
+          type: "divider"
+        },
+        {
+          type: "option",
+          label: "Leave Project",
+          onClick: () => console.log("something")
+        }
+      ]}
+    >
+      <StyledProjectCard>
+        <ProjectMetaContainer>
+          <ProjectTitle>
+            {title}
+          </ProjectTitle>
+          <ProjectDescription>{description}</ProjectDescription>
+        </ProjectMetaContainer>
+        <ProjectActionsContainer>
+          <DropdownArrow />
+        </ProjectActionsContainer>
+      </StyledProjectCard>
+    </PopoverMenu>
   );
 };
 
