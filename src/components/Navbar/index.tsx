02f84c70176f63193615230eaa294f9f64f1ddd1
work <madsborpe@gmail.com>
2020-04-23 11:12:27 +0200

    update change to FC

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,80 +1,68 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { StoreState } from "../../reducers";
-import _, { isEmpty } from "lodash";
-import { AuthData, ProjectData, fetchProjects, showModal } from "../../actions";
-import Tooltip from "../Tooltip";
-import { ReactComponent as ProjectIconDefault } from "../../assets/svg/ProjectIconDefault.svg";
-import {
-  StyledNavbar,
-  ProjectLinkContainer,
-  AddProjectIcon,
-  ProjectLink
-} from "./style";
+import React from 'react';
+import { connect } from 'react-redux';
+import { StoreState } from '../../reducers';
+import _ from 'lodash';
+import { AuthData, ProjectData, fetchProjects, showModal } from '../../actions';
+import Tooltip from '../Tooltip';
+import { ReactComponent as ProjectIconDefault } from '../../assets/svg/ProjectIconDefault.svg';
+import { StyledNavbar, ProjectLinkContainer, AddProjectIcon, ProjectLink } from './style';
 
 interface Props {
   projects: ProjectData[];
   auth: AuthData;
   fetchProjects: Function;
-  showModal: Function;
+  showModal: typeof showModal;
 }
 
-class Navbar extends Component<Props> {
-  renderProjectLinks() {
-    return this.props.projects.map((doc: ProjectData, i) => {
+const Navbar: React.FC<Props> = ({ projects, showModal }: Props) => {
+  const renderProjectLinks = () => {
+    return projects.map((doc: ProjectData, i) => {
       return (
         <Tooltip content={doc.title} placement="right" key={i}>
-          <ProjectLink
-            activeClassName="selected"
-            to={`/${doc.slug}`}
-            key={doc.slug}
-          >
+          <ProjectLink activeClassName="selected" to={`/${doc.slug}`} key={doc.slug}>
             <ProjectIconDefault />
           </ProjectLink>
         </Tooltip>
       );
     });
-  }
-
-  render() {
-    return (
-      <StyledNavbar>
-        <ProjectLinkContainer>
-          {this.renderProjectLinks()}
-          {_.isEmpty(this.props.projects) ? (
-            <Tooltip
-              content="Create a new project to start creating Flows"
-              placement="right"
-              visible
-              key={1}
-              hideOnClick={false}
-            >
-              <AddProjectIcon
-                onClick={() =>
-                  this.props.showModal({
-                    modalProps: { open: true },
-                    modalType: "CREATE_PROJECT_MODAL"
-                  })
-                }
-              />
-            </Tooltip>
-          ) : (
-            <Tooltip content="Create a new project" placement="right" key={2}>
-              <AddProjectIcon
-                onClick={() =>
-                  this.props.showModal({
-                    modalProps: { open: true },
-                    modalType: "CREATE_PROJECT_MODAL"
-                  })
-                }
-              />
-            </Tooltip>
-          )}
-        </ProjectLinkContainer>
-      </StyledNavbar>
-    );
-  }
-}
+  };
+  return (
+    <StyledNavbar>
+      <ProjectLinkContainer>
+        {renderProjectLinks()}
+        {_.isEmpty(projects) ? (
+          <Tooltip
+            content="Create a new project to start creating Flows"
+            placement="right"
+            visible
+            key={1}
+            hideOnClick={false}
+          >
+            <AddProjectIcon
+              onClick={() =>
+                showModal({
+                  modalProps: { open: true },
+                  modalType: 'CREATE_PROJECT_MODAL'
+                })
+              }
+            />
+          </Tooltip>
+        ) : (
+          <Tooltip content="Create a new project" placement="right" key={2}>
+            <AddProjectIcon
+              onClick={() =>
+                showModal({
+                  modalProps: { open: true },
+                  modalType: 'CREATE_PROJECT_MODAL'
+                })
+              }
+            />
+          </Tooltip>
+        )}
+      </ProjectLinkContainer>
+    </StyledNavbar>
+  );
+};
 
 const mapStateToProps = ({ auth, projects }: StoreState) => {
   return { auth, projects: Object.values(projects.items) };
